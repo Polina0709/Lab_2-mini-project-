a6f13e58bedd1909119ae4825f6808074a4a7fa3
Melnik Polina <125994990+Polina0709@users.noreply.github.com>
2023-12-14 14:00:44 +0200

    Update MainViewViewModel.swift

--- ViewModels/MainViewViewModel.swift
+++ ViewModels/MainViewViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+class MainViewViewMode: ObservableObject {
+    @Published var currentUserId: String = ""
+    private var handler: AuthStateDidChangeListenerHandle?
+    
+    init() {
+        self.handler = Auth.auth().addStateDidChangeListener { [weak self]_, user in
+            DispatchQueue.main.async {
+                self?.currentUserId = user?.uid ?? ""
+            }
+        }
+    }
+    public var isSignedIn: Bool {
+        return Auth.auth().currentUser != nil
+    }
+}
