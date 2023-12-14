e28f93da2cf4c01f9c481f464d0f7c4731bca9db
Melnik Polina <125994990+Polina0709@users.noreply.github.com>
2023-12-14 13:59:40 +0200

    Update MainView.swift

--- Views/MainView.swift
+++ Views/MainView.swift
@@ -4,14 +4,17 @@
 //
 //  Created by Polya Melnik on 13.12.2023.
 //
-
 import SwiftUI
 
 struct MainView: View {
+    @StateObject var viewModel = MainViewViewMode()
+    
     var body: some View {
-        NavigationView {
+        if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
+            //signed in
+            ToDoListView()
+        } else {
             LoginView()
-            Spacer()
         }
     }
 }
